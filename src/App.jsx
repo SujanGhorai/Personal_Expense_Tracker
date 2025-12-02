fc44eec9db5bd619d9fb07c12e64cf8633476a52
SujanGhorai <sujan33ghorai@gmail.com>
2025-12-02 23:10:36 +0530

    update

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Analytics from "./pages/Analytics";
+import ExpenseHistory from "./sections/ExpenseHistory";
+import NotFoundPage from "./pages/NotFoundPage";
+import { ExpenseProvider } from "./context/context";
+
+function App() {
+    // useEffect(() => {
+    //     document.addEventListener("contextmenu", (e) => e.preventDefault());
+    //     document.addEventListener("keydown", (e) => {
+    //         if (
+    //             e.key === "F12" ||
+    //             (e.ctrlKey && e.shiftKey && e.key === "I") ||
+    //             (e.ctrlKey && e.shiftKey && e.key === "C") ||
+    //             (e.ctrlKey && e.shiftKey && e.key === "J") ||
+    //             (e.ctrlKey && e.key == "U")
+    //         ) {
+    //             e.preventDefault();
+    //         }
+    //     });
+    // }, []);
+
+    return (
+        <BrowserRouter>
+            <ExpenseProvider>
+                <Routes>
+                    <Route element={<Layout />}>
+                        <Route path="/" element={<Analytics />} />
+                        <Route path="/history" element={<ExpenseHistory />} />
+                    </Route>
+                    <Route path="*" element={<NotFoundPage />} />
+                </Routes>
+            </ExpenseProvider>
+        </BrowserRouter>
+    );
+}
+
+export default App;
